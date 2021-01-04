940f67b3f2e15c73bb63f392c8ee998921e6d4dd
devCordain <pctkar@gmail.com>
2021-01-04 15:35:55 +0100

    initial commit

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
